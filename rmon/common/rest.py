f1fd871360e7c93c9d90586f120d0ea4178d7252
lingxiaojun <2760883684@qq.com>
2018-10-24 11:52:54 +0800

    Third Blood

--- rmon/common/rest.py
+++ rmon/common/rest.py
@@ -1,5 +1,10 @@
 """ rmon.common.rest
 """
+from collections import Mapping
+from flask import request, make_response
+from flask.json import dumps
+from flask.views import MethodView
+from Werkzeug.wrappers import Response
 
 class RestException(Exception):
     """异常基类
@@ -15,3 +20,73 @@ class RestException(Exception):
         self.code = code
         self.message = message
         super(RestException, self).__init__()
+
+class RestView(MethodView):
+
+    content_type = 'application/json; charset=utf-8'
+    method_decorators = []
+
+    def handler_error(self, exception):
+
+        data = {
+                'ok': False,
+                'message': exception.message
+                }
+
+        result = dumps(data) + '\n'
+        resp = make_response(result, exception.code)
+        resp.headers['Content-Type'] = self.content_type
+        return resp
+
+    def dispatch_request(self, *args, **kwargs):
+        method = getattr(self, request.method.lower(), None)
+        if method is None and request.method == 'HEAD':
+            method = getattr(self, 'get', None)
+
+        assert method is not None, 'Unimplemented method %r' % request.method
+
+        if isinstance(self.method_decorators, Mapping):
+            decorators = self.method_decorators.get(request.method.lower(), [])
+        else:
+            decorators = self.method_decorators
+
+        for decorator in decorators:
+            method = decorator(method)
+
+        try:
+            resp = method(*args, **kwargs)
+        except RestException as e:
+            resp = self.handler_error(e)
+
+        if isinstance(resp, Response):
+            return resp
+
+        data, code, headers = RestView.unpack(resp)
+
+        if code >= 400 and isinstance(data, dict):
+            for key in data:
+                if isinstance(data[key], list) and len(data[key]) > 0:
+                    message = data[key][0]
+                else:
+                    message = data[key]
+            data = {'ok': False, 'message': message}
+
+        result = dumps(data) + '\n'
+        response = make_response(result, code)
+        response.headers.extend(headers)
+
+        response.headers['Content-Type'] = self.content_type
+        return response
+
+    @staticmethod
+    def unpack(value):
+        headers = {}
+        if not isinstance(value, tuple):
+            return value, 200, {}
+        if len(value) == 3:
+            data, code, headers = value
+        elif len(value) == 2:
+            data, code = value
+        return data, code, headers
+
+
--- rmon/models.py
+++ rmon/models.py
@@ -5,6 +5,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis, RedisError
 
+from marshmallow import (Schema, fields, validate, post_load, validates_schema, ValidationError)
+
 from datetime import datetime
 
 from rmon.common.rest import RestException
@@ -66,3 +68,57 @@ class Server(db.Model):
             return self.redis.info()
         except RedisError:
             raise RestException(400, 'redis server %s can not connected' % self.host)
+
+    def execute(self, *args, **kwargs):
+
+        pass
+
+class ServerSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    name = fields.String(required=True, validate=validate.Length(2, 64))
+    description = fields.String(validate=validate.Length(0, 512))
+
+    host = fields.String(required=True,
+            validate=validate.Regexp(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$'))
+    port = fields.Integer(validate=validate.Range(1024, 65536))
+    password = fields.String()
+    updated_at = fields.DateTime(dump_only=True)
+    created_at = fields.DateTime(dump_only=True)
+
+    @validates_schema
+    def validate_schema(self, data):
+
+        if 'port' not in data:
+            data['port'] = 6379
+
+        instance = self.context.get('instance', None)
+
+        server = Server.query.filter_by(name=data['name']).first()
+
+        if server is None:
+            return
+
+        if instance is not None and server != instance:
+            raise ValidationError('Redis server already exist', 'name')
+
+        if instance is None and server:
+            raise ValidationError('Redis server already exist', 'name')
+
+    @post_load
+    def create_or_update(self, data):
+
+        instance = self.context.get('instance', None)
+
+        if instance is None:
+            return Server(**data)
+
+        for key in data:
+            setattr(instance, key, data[key])
+        return instance
+
+
+
+
+
+
+
